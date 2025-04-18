bb3306a6f7af61ea607d645dd4dfdc3411e79a9a
mehtavandit <vanditmehta303@gmail.com>
2025-04-18 14:49:00 -0400

    Solved secrets detected

--- firebase/client.ts
+++ firebase/client.ts
@@ -8,7 +8,7 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBzb29JL04vdU7PaRvNB--dQq-Ux1MGG-o",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: "speakcruit.firebaseapp.com",
   projectId: "speakcruit",
   storageBucket: "speakcruit.firebasestorage.app",
