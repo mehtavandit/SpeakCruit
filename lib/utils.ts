104e6b92ec4338519abb23292be9d46db1266488
mehtavandit <vanditmehta303@gmail.com>
2025-04-19 15:27:42 -0400

    Implemented Interview Generation

--- lib/utils.ts
+++ lib/utils.ts
@@ -6,7 +6,10 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-const techIconBaseURL = "https://cdn.jsdelivr.net/gh/devicons/devicon/icons";
+// const techIconBaseURL = "https://cdn.jsdelivr.net/gh/devicons/devicon/icons";
+
+const techIconBaseURL = "https://cdn.jsdelivr.net/gh/devicons/devicon@v2.16.0/icons";
+
 
 const normalizeTechName = (tech: string) => {
   const key = tech.toLowerCase().replace(/\.js$/, "").replace(/\s+/g, "");
